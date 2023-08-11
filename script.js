57978011c7f05a3015808dc25a8806d3b58aeeff
eval-u8 <59159726+eval-u8@users.noreply.github.com>
2023-08-10 20:38:09 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,37 +1,26 @@
 let inputEl = document.getElementById("input-el");
 let outputEl = document.getElementById("output-el");
-console.log(inputEl);
 
 function splitfn() {
-  outputEl.innerHTML = "";
+  // outputEl.innerHTML = "";
   let str = inputEl.value;
-  console.log(str);
   let str2 = str.split("|");
-  console.log(str2);
   let newP = document.createElement("p");
-  let newC = document.createTextNode(str2[2]);
-  newP.appendChild(newC);
-  outputEl.appendChild(newP);
-  newP = document.createElement("p");
-  newC = document.createTextNode(str2[4]);
-  newP.appendChild(newC);
-  outputEl.appendChild(newP);
-  newP = document.createElement("p");
-  newC = document.createTextNode(str2[12]);
-  newP.appendChild(newC);
-  outputEl.appendChild(newP);
-  newP = document.createElement("p");
-  newC = document.createTextNode(str2[10]);
-  newP.appendChild(newC);
+  newP.innerHTML =
+    str2[4] + "  -  " + str2[2] + "  -  " + str2[10] + "  -  Qty: " + str[12];
   outputEl.appendChild(newP);
   inputEl.value = "";
-  copy();
+}
+
+function clearContent() {
+  outputEl.innerHTML = "";
+  inputEl.value = "";
 }
 
 const copy = async () => {
   try {
     await navigator.clipboard.writeText(outputEl.innerText);
-    console.log("Content copied to clipboard");
+    alert("Content copied to clipboard");
   } catch (err) {
     console.error("Failed to copy: ", err);
   }
